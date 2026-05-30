f9cf5977669917e624bbc3ea03940c936445ffc6
Gelo <mapanis@gbox.adnu.edu.ph>
2026-05-30 21:24:44 +0800

    Day 1
    Day 1 setting up the product lists

--- python/list.py
+++ python/list.py
@@ -1,1 +1,14 @@
+products = [
+    {"Product": "Coke", "Price": 25, "Stock": 50},
+    {"Product": "Sprite", "Price": 25, "Stock": 50},
+    {"Product": "Royal", "Price": 25, "Stock": 50},
+    {"Product": "Pepsi", "Price": 25, "Stock": 50},
+    {"Product": "Mountain Dew", "Price": 25, "Stock": 50},
+    {"Product": "Dr. Pepper", "Price": 25, "Stock": 50},
+]
 
+for product in products:
+    print(product["Product"], "-", product["Stock"], "left")
+    
+    if product["Stock"] <= 10:
+        print("Low Stock Alert:", product["Product"])
