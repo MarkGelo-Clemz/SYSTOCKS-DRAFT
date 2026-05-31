c455a0fb61260eea530a357319125386d73c232c
Gelo <markclemz86@gmail.com>
2026-05-31 21:35:52 +0800

    Day 2 Options add and remove

--- python/list.py
+++ python/list.py
@@ -8,8 +8,43 @@ products = [
     {"Product": "Dr. Pepper", "Price": 25, "Stock": 50},
 ]
 
-for product in products:
-    print(product["Product"], "-", product["Stock"], "left")
+
+
+choice = ""
+while choice != "4":
+    print("=========================================")
+    print("1. View Products")
+    print("2. Add Product")
+    print("3. Remove Product")
+    print("4. Exit")
+    print("=========================================")
+    choice = input("Enter choice: ")
     
-    if product["Stock"] <= 10:
-        print("Low Stock Alert:", product["Product"])
+    if choice == "1":
+        print("=========================================")
+        for product in products:
+            print(product["Product"], "-", product["Stock"], "left")
+        
+            if product["Stock"] <= 10:
+                print("Low Stock Alert:", product["Product"])
+
+    elif choice == "2":
+        print("=========================================")
+        name = input("Input Product Name: ")
+        price = float(input("Input Product Price: "))
+        stock = int(input("Input Product Quantity: "))
+        new_product = {"Product": name, "Price": price, "Stock": stock}
+        products.append(new_product)
+        
+    elif choice == "3":
+        print("=========================================")
+        remove = input("Input product to remove: ")
+
+        for product in products:
+            if product["Product"].lower() == remove.lower():
+                products.remove(product)
+
+    
+
+
+
