d91b0c78d76a8f56403cb4fb1cc42f34ddf046a8
Gelo <markclemz86@gmail.com>
2026-06-02 23:54:04 +0800

    day 3 functions

--- python/list.py
+++ python/list.py
@@ -8,6 +8,30 @@ products = [
     {"Product": "Dr. Pepper", "Price": 25, "Stock": 50},
 ]
 
+def view_products():
+    print("=========================================")
+    for product in products:
+        print(product["Product"], "-", product["Stock"], "left")
+        
+        if product["Stock"] <= 10:
+            print("Low Stock Alert:", product["Product"])
+
+def add_product():
+    print("=========================================")
+    name = input("Input Product Name: ")
+    price = float(input("Input Product Price: "))
+    stock = int(input("Input Product Quantity: "))
+    new_product = {"Product": name, "Price": price, "Stock": stock}
+    products.append(new_product)
+
+
+def remove_product():
+    print("=========================================")
+    remove = input("Input product to remove: ")
+    for product in products:
+            if product["Product"].lower() == remove.lower():
+                products.remove(product)
+
 
 
 choice = ""
@@ -20,31 +44,16 @@ while choice != "4":
     print("=========================================")
     choice = input("Enter choice: ")
     
+
+
     if choice == "1":
-        print("=========================================")
-        for product in products:
-            print(product["Product"], "-", product["Stock"], "left")
-        
-            if product["Stock"] <= 10:
-                print("Low Stock Alert:", product["Product"])
+        view_products()
 
     elif choice == "2":
-        print("=========================================")
-        name = input("Input Product Name: ")
-        price = float(input("Input Product Price: "))
-        stock = int(input("Input Product Quantity: "))
-        new_product = {"Product": name, "Price": price, "Stock": stock}
-        products.append(new_product)
+        add_product()
         
     elif choice == "3":
-        print("=========================================")
-        remove = input("Input product to remove: ")
+        remove_product()
 
-        for product in products:
-            if product["Product"].lower() == remove.lower():
-                products.remove(product)
-
-    
-
-
-
+    elif choice == "4":
+        print("Goodbye")
