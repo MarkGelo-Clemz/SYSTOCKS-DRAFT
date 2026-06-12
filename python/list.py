098a55e5e5324de0ed487b2ec1dd98633021f867
Gelo <markclemz86@gmail.com>
2026-06-12 23:13:02 +0800

    DAY 8 with errors to be fixed

--- python/list.py
+++ python/list.py
@@ -1,6 +1,10 @@
 #Day 1- prodduct list with low stock a
 import sqlite3
 import os
+import datetime
+
+date = datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+
 conn = sqlite3.connect("magic.db") #a some type of fstream
 cursor = conn.cursor() #a pen where ables you to write commands
 
@@ -8,6 +12,9 @@ cursor = conn.cursor() #a pen where ables you to write commands
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
+
+#PRODUCT LISTS =================================================
 cursor.execute(""" 
 CREATE TABLE IF NOT EXISTS product (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,6 +22,7 @@ CREATE TABLE IF NOT EXISTS product (
             price REAL,
             stock INTEGER)
             """) #this is like a template for the db
+
 
 
 products = [
@@ -29,12 +37,96 @@ products = [
 
 cursor.execute("SELECT COUNT (*) FROM product")
 count = cursor.fetchone()[0]
+    
+
+
 
 if count == 0:
     for product in products:
         cursor.execute("INSERT INTO product (product, price, stock) VALUES (?, ?, ?)",
                     (product["Product"], product["Price"], product["Stock"]))
 conn.commit()
+#PRODUCT LISTS =================================================
+
+
+
+
+
+#USERS =================================================
+#ERROR HERE DOESNT CREATE THE TABLE
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT,
+            password TEXT,
+            role TEXT)
+            """)
+
+users = [
+    {"Username": "Staff1", "Password": "1234", "role": "staff"},
+    {"Username": "Manager1", "Password": "admin", "role": "manager"},
+        ]
+
+cursor.execute("SELECT COUNT (*) FROM users")
+count = cursor.fetchone()[0]
+
+if count == 0:
+    for user in users:
+        cursor.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
+                    (user["Username"], user["Password"], user["role"]))
+conn.commit()
+
+def login():
+    print("====================SYSTOCKS LOGIN====================")
+    username = input("Username: ").lower()
+    password = input("Password: ").lower()
+
+    cursor.execute("SELECT role FROM users WHERE LOWER(username) = LOWER(?) AND LOWER(password) = LOWER(?)", (username, password))
+    result = cursor.fetchone()
+
+    if result is None:
+        print("Invald Credentials!")
+        return None
+    
+    return result[0]
+
+
+#USERS =================================================
+
+
+
+
+
+
+#SALES =================================================================================
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS sales(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product TEXT
+            quantity INTEGER,
+            price REAL,
+            toal REAL,
+            dat TEXT)
+            """)
+
+
+
+
+
+
+
+
+
+#SALES =================================================================================
+
+
+
+
+
+
+
+#FUNCTIONS ================================================================
 
 def view_products():
     cursor.execute("SELECT * FROM product ORDER BY product") #gathers and red the data 
@@ -62,7 +154,7 @@ def add_product(products):
     if count == 1:
         print("Product already exists")
         add = int(input("How many stock to add? (0 to back): "))
-        if add == "0":
+        if add == 0:
             return 
         cursor.execute("UPDATE product SET stock = stock + ? WHERE LOWER(product) = LOWER(?)", (add, name))
         conn.commit()
@@ -82,63 +174,111 @@ def remove_product(products):
     cursor.execute("DELETE FROM product WHERE LOWER(product) = LOWER(?)", (remove,)) #to delete command LOWER or UPPER for replacement
     conn.commit()
 
+
+# WE HAVE ERROR HERE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 def reduce_quan(products):
     print("=========================================")
     name = input("Input product name (0 to back): ")
     if name == "0":
         return
-    for product in products:
-            if product["Product"].lower() == name.lower():
-                reduce = int(input("How many products to be bought? (0 to back): "))
-                if reduce == "0":
-                    return
+    
+    cursor.execute("SELECT stock FROM product WHERE LOWER(product) = LOWER(?)", (name,))
+    result = cursor.fetchone()
+
+    if result is None:
+        print("Product not found.")
+        return
+
+        current_stock = result[0]
+        reduce = int(input("How many products to be bought? (0 to back): "))
+        if reduce == 0:
+            return
 
 
-                if reduce <= product["Stock"]:
-                    product["Stock"] = product["Stock"] - reduce
+        if reduce <= product["Stock"]:
+            product["Stock"] = product["Stock"] - reduce
+            cursor.execute("UPDATE product SET stock = ? WHERE LOWER(product) = LOWER(?)", (product["Stock"], name))
+            conn.commit()
+            print("Stock Updated")
 
-                else:
-                    print("Above Threshold!")
+        else:
+            print("Above Threshold!")
                 
-                cursor.execute("UPDATE product SET stock = ? WHERE LOWER(product) = LOWER(?)", (product["Stock"], name))
-                conn.commit()
-                
-
+#FUNCTIONS ================================================================
             
 
 
 
+#MENU ============================================================================
+role = None
+while role is None:
+    clear()
+    role = login()
 
 choice = ""
 while choice != "0":
 
-    print("=========================================")
-    print("1. View Products")
-    print("2. Add Product")
-    print("3. Remove Product")
-    print("4. Reduce Quantity")
-    print("0. Exit")
-    print("=========================================")
-    choice = input("Enter choice: ")
-    
-
-
-    if choice == "1":
-        clear()
-        view_products()
-
-    elif choice == "2":
-        clear()
-        add_product(products)
+    if role == "manager":
+        print("=========================================")
+        print("SYSTOCKS - MANAGER")
+        print("=========================================")
+        print("1. View Products")
+        print("2. Add Product")
+        print("3. Remove Product")
+        print("4. Reduce Quantity")
+        print("0. Exit")
+        print("=========================================")
+        choice = input("Enter choice: ")
         
-    elif choice == "3":
-        clear()
-        remove_product(products)
+        if choice == "1":
+            clear()
+            view_products()
+            
+        elif choice == "2":
+            clear()
+            add_product(products)
+            
+        elif choice == "3":
+            clear()
+            remove_product(products)
 
-    elif choice == "4":
-        clear()
-        reduce_quan(products)
+        elif choice == "4":
+            clear()
+            reduce_quan(products)
 
-    elif choice == "0":
-        clear()
-        print("Goodbye")
+        elif choice == "0":
+            clear()
+            print("Goodbye")
+    
+    elif role == "staff":
+
+        print("=========================================")
+        print("SYSTOCKS - STAFF")
+        print("=========================================")
+        print("1. View Products")
+        print("2. Reduce Quantity")
+        print("0. Exit")
+        print("=========================================")
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            clear()
+            view_products()
+
+        elif choice == "4":
+            clear()
+            reduce_quan(products)
+
+        elif choice == "0":
+            clear()
+            print("Goodbye")
+
+
+#MENU ============================================================================
+
+
+
+
+
+
+
