b4ad1b7c5c8d0681406f442605c358c11c8e1c20
Gelo <markclemz86@gmail.com>
2026-06-28 21:50:53 +0800

    STREAMLIT

--- python/app.py
+++ python/app.py
@@ -15,42 +15,40 @@ page = st.sidebar.selectbox("Choose", ["View Products", "Add Products", "Remove 
 if page == "View Products":
     cursor.execute("SELECT * FROM product ORDER BY product") #gathers and red the data 
     results = cursor.fetchall()
-    
-    
-
-    print("=========================================")
-
-    for row in results:
-        print(row[1], "-", row[2], "-", row[3], "left")
-        
-        if row[3] <= 10:
-            print("Low Stock Alert:", row[1])
+    df = pd.DataFrame(results, columns=["ID", "Product", "Price", "Stock"])
+    st.dataframe(df)
 
 elif page == "Add Products":
-    print("=========================================")
-    name = input("Input Product Name (0 to back): ").capitalize()
     
+    name = st.text_input("Input Product Name (0 to back): ").capitalize()
 
-    cursor.execute("SELECT COUNT(*) FROM product WHERE LOWER(product) = LOWER(?)", (name,))
-    count = cursor.fetchone()[0] #grabs result just 1
+    if st.button("Check Product"):
+        cursor.execute("SELECT COUNT(*) FROM product WHERE LOWER(product) = LOWER(?)", (name,))
+        count = cursor.fetchone()[0] #grabs result just 1
+
+        if count == 1:
+            st.warning("Product already exists")
+            add = st.number_input("How many stock to add? (0 to back): ")
+
+            if st.button("Add Stock"):
+                cursor.execute("UPDATE product SET stock = stock + ? WHERE LOWER(product) = LOWER(?)", (add, name))
+                conn.commit()
+                st.success("Stock Updated!")
+
+        else:
+            st.info("New Product! Fill in details:")
+            price = st.number_input("Input Product Price: ")
+            stock = st.number_input("Input Product Quantity: ")
+
+            if st.button("Add Product"):
+                cursor.execute("INSERT INTO product (product, price, stock) VALUES (?, ?, ?)", (name, price, stock))
+                conn.commit()
+                st.success("Product Added!")
 
 
-    if count == 1:
-        print("Product already exists")
-        add = int(input("How many stock to add? (0 to back): "))
-        
-        cursor.execute("UPDATE product SET stock = stock + ? WHERE LOWER(product) = LOWER(?)", (add, name))
-        conn.commit()
-
-    else:
-        price = float(input("Input Product Price: "))
-        stock = int(input("Input Product Quantity: "))
-        cursor.execute("INSERT INTO product (product, price, stock) VALUES (?, ?, ?)", (name, price, stock))
-        conn.commit()
 
 elif page == "Remove Products":
-    print("=========================================")
-    remove = input("Input product to remove (0 to back): ")
+    remove = st.text_input("Input product to remove (0 to back): ")
     
     confirm = input("Are you sure you want to remove " + remove + "? (y/n)").lower()
     if confirm == 'y':
