a9c29f7a04f9a866df2384a022ccf4912341cca0
Gelo <markclemz86@gmail.com>
2026-06-28 22:33:27 +0800

    STREAMLIT UPDATED

--- python/app.py
+++ python/app.py
@@ -20,40 +20,60 @@ if page == "View Products":
 
 elif page == "Add Products":
     
-    name = st.text_input("Input Product Name (0 to back): ").capitalize()
+    name = st.text_input("Input Product Name: ").capitalize()
 
     if st.button("Check Product"):
         cursor.execute("SELECT COUNT(*) FROM product WHERE LOWER(product) = LOWER(?)", (name,))
         count = cursor.fetchone()[0] #grabs result just 1
+        st.session_state["count"] = count #save
+        st.session_state["pname"] = name
 
-        if count == 1:
+
+    if "count" in st.session_state:
+        if st.session_state["count"] == 1:
             st.warning("Product already exists")
             add = st.number_input("How many stock to add? (0 to back): ")
 
             if st.button("Add Stock"):
                 cursor.execute("UPDATE product SET stock = stock + ? WHERE LOWER(product) = LOWER(?)", (add, name))
                 conn.commit()
-                st.success("Stock Updated!")
+                st.write("Stock Updated!")
+                st.session_state.clear()
+                st.rerun
 
         else:
-            st.info("New Product! Fill in details:")
+            st.info("New Product! Fill in details!")
             price = st.number_input("Input Product Price: ")
             stock = st.number_input("Input Product Quantity: ")
 
             if st.button("Add Product"):
                 cursor.execute("INSERT INTO product (product, price, stock) VALUES (?, ?, ?)", (name, price, stock))
                 conn.commit()
-                st.success("Product Added!")
-
+                st.write("Product Added!")
+                st.session_state.clear()
+                st.rerun
 
 
 elif page == "Remove Products":
-    remove = st.text_input("Input product to remove (0 to back): ")
+    remove = st.text_input("Input product to remove: ")
+    if st.button("Remove Product"):
+        st.session_state["remove_name"] = remove
     
-    confirm = input("Are you sure you want to remove " + remove + "? (y/n)").lower()
-    if confirm == 'y':
-        cursor.execute("DELETE FROM product WHERE LOWER(product) = LOWER(?)", (remove,)) #to delete command LOWER or UPPER for replacement
-        conn.commit()
+    if "remove_name" in st.session_state:
+        st.warning("Are you sure you want to remove " + remove)
+        col1, col2 = st.columns(2)
+
+        with col1:
+            if st.button("Yes"):
+                cursor.execute("DELETE FROM product WHERE LOWER(product) = LOWER(?)", (st.session_state["remove_name"],)) #to delete command LOWER or UPPER for replacement
+                conn.commit()
+                st.session_state.clear()
+                st.info("Product Succesfully Removed!")
+
+        with col2:
+            if st.button("Cancel"):
+                st.session_state.clear()
+                st.rerun()
         
 
 
